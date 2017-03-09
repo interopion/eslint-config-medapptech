a1052d0605bca356292878745f95b2876892c51c
Veselin <veselin@Veselins-MacBook-Pro.local>
2017-03-09 04:16:36 +0200

    Missing props validation ignores ‘children’ prop.

--- frontend-test.js
+++ frontend-test.js
@@ -185,7 +185,7 @@ module.exports = {
 
         // Prevent missing props validation in a React component definition
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-        'react/prop-types': ['error', { ignore: [], customValidators: [] }],
+        'react/prop-types': ['error', { ignore: [ 'children' ], customValidators: [] }],
 
         // Prevent missing React when using JSX
         // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
